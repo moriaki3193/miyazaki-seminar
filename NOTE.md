1162a8dbb42eddb2836e3563764781f900b26b87
moriaki3193 <puppis1221@gmail.com>
2016-01-01 02:32:55 +0900

    8th commit | GemfileにRedcarpetを追記しbundle install

--- NOTE.md
+++ NOTE.md
@@ -1,3 +1,5 @@
 # のちに実装することリスト
 * shared/_header.html.erbのロゴimg
 * shared/_header.html.erbのliをeachとかでまわす
+
+* DBをmysql2に差し替える
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
     @title = "Miyazaki Seminar"
-    @products = Product.all
   end
 end
