b771c8af09d5c7accf3bdcf62516aedf39cab462
moriaki3193 <puppis1221@gmail.com>
2016-01-07 02:16:34 +0900

    14th commit | 簡単な議事録の登録機能まで

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,14 +4,29 @@ class NotesController < ApplicationController
   end
 
   def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to notes_path(params[:id])
+    else
+      # ValidationエラーなどでDBに保存できない場合new.html.erbを再表示
+      render 'new'
+    end
   end
 
   def show
-    id = params[:id]
+    @note = Note.find(params[:id])
   end
 
   def update
-
     redirect_to :action => "index"
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :content, :written_by)
   end
 end
